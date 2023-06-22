5fda6672da6ce2ee4d5b75344bec1952801aa524
njmarti9 <njmarti9@asu.edu>
2023-06-21 17:32:12 -0700

    Finished the test cases for this exercise - Used nesting for loop to run through array and arguements - Had to clone slice new array so 1st wouldn't change as well

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,18 @@
-const repeatString = function() {
-
+const repeatString = function(word, amount) {
+    let result = "";
+    if (amount === 0)
+    {
+        return ''
+    }
+    else if (amount < 0) 
+    {
+        return 'ERROR'
+    }
+    for (let i = 0; i < amount; i++)
+    {
+        result += word;
+    }
+    return result
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,24 @@
-const removeFromArray = function() {
+const removeFromArray = function(inputArray) {
+    // Create new array to not effect the loops
+    newArray = inputArray.slice();
+    // Keep track of slices so newArray slice is accurate
+    let sliceAmount = 0;
 
+    // Loop through the original array and then the arguments to check for removal
+    for (let i = 0; i < inputArray.length; i++)
+    {
+        for (let j = 1; j < arguments.length; j++)
+        {   
+            if (inputArray[i] === arguments[j])
+            {
+                newArray.splice(i-sliceAmount, 1);
+                sliceAmount++;
+                break
+            }
+        }
+    }
+    // Return modified array
+    return newArray
 };
 
 // Do not edit below this line
